dfeb3e6163f281cbcb286fecc66ba29adb545189
Chris Stone <chstone@microsoft.com>
2017-04-05 11:35:21 -0400

    remove comment

--- spec/helpers/reporters.js
+++ spec/helpers/reporters.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const jasmineReporters = require('jasmine-reporters');
 
-// jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter());
 jasmine.getEnv().addReporter(new jasmineReporters.NUnitXmlReporter({
   savePath: process.env.COMMON_TESTRESULTSDIRECTORY || path.resolve(__dirname, '..', '..'),
 }));
