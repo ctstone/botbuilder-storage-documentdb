7b74d42f57771530c48e471264a584d88b360150
Chris Stone <chstone@microsoft.com>
2017-04-05 10:46:08 -0400

    changes for tslint

--- src/documentdb-bot-storage.ts
+++ src/documentdb-bot-storage.ts
@@ -6,14 +6,14 @@ const ONE_WEEK_IN_SECONDS = 604800;
 const DEFAULT_THROUGHPUT = 20000;
 const HTTP_CONFLICT = 409;
 
-type DocumentDbCallback = (err:QueryError, ...args:any[]) => void;
+type DocumentDbCallback = (err: QueryError, ...args: any[]) => void;
 
 export class DocumentDbBotStorage implements IBotStorage {
 
     private maxConcurrency: number;
     private initialization: boolean|QueryError;
     private partitioned: boolean;
-    private pendingInit: Array<(err: QueryError, ...args:any[]) => void> = [];
+    private pendingInit: Array<(err: QueryError, ...args: any[]) => void> = [];
 
     /**
      * Create new DocumentDbBotStorage
@@ -36,7 +36,7 @@ export class DocumentDbBotStorage implements IBotStorage {
       }
 
     getData(context: IBotStorageContext, callback: (err: Error, data: IBotStorageData) => void): void {
-      const keys: {userData?:any, privateConversationData?:any, conversationData?:any} = {};
+      const keys: {userData?: string, privateConversationData?: string, conversationData?: string} = {};
       if (context.userId) {
         if (context.persistUserData) {
           keys.userData = this.userId(context);
@@ -48,49 +48,49 @@ export class DocumentDbBotStorage implements IBotStorage {
       if (context.persistConversationData && context.conversationId) {
         keys.conversationData = this.conversationId(context);
       }
-      
+
       async.waterfall([
         (next: (err: QueryError) => void) => this.init(next),
         (next: (err: QueryError) => void) => this.readData(keys, next),
-      ], this.mapDocumentDbError(callback))
+      ], this.mapDocumentDbError(callback));
     }
 
     saveData(context: IBotStorageContext, data: IBotStorageData, callback?: (err: Error) => void): void {
-      const docs: {id:string, data:any}[] = [];
+      const docs: Array<{id: string, data: any}> = [];
       if (context.userId) {
         if (context.persistUserData) {
           docs.push({ id: this.userId(context), data: data.userData });
         }
         if (context.conversationId) {
-          docs.push({ id:this.privateConversationId(context), data: data.privateConversationData });
+          docs.push({ id: this.privateConversationId(context), data: data.privateConversationData });
         }
       }
       if (context.persistConversationData && context.conversationId) {
         docs.push({ id: this.conversationId(context), data: data.conversationData });
       }
-      
+
       async.series([
         (next: (err: QueryError) => void) => this.init(next),
         (next: (err: QueryError) => void) => this.writeData(docs, next),
       ], this.mapDocumentDbError(callback));
     }
 
-    private readData(keys: {userData?:any, privateConversationData?:any, conversationData?:any}, callback: (err: QueryError, data: IBotStorageData) => void): void {
+    private readData(keys: {userData?: string, privateConversationData?: string, conversationData?: string}, callback: (err: QueryError, data: IBotStorageData) => void): void {
       async.mapValuesLimit(keys, this.maxConcurrency, (docId, type, next) => {
         const partitionKey = this.partitioned ? docId : null;
         const docLink = `dbs/${this.databaseName}/colls/${this.collectionName}/docs/${docId}`;
         async.waterfall([
-          (next:any) => this.client.readDocument(docLink, { partitionKey:partitionKey }, next),
-          (resource:any, headers:any, next:any) => next(null, resource.data),
+          (next: any) => this.client.readDocument(docLink, { partitionKey }, next),
+          (resource: any, headers: any, next: any) => next(null, resource.data),
         ], next);
       }, callback);
     }
 
-    private writeData(docs: Array<{id:string, data:any}>, callback: (err: QueryError) => void): void {
+    private writeData(docs: Array<{id: string, data: any}>, callback: (err: QueryError) => void): void {
       async.eachLimit(docs, this.maxConcurrency, (doc, next: (err: QueryError) => void) => {
         const partitionKey = this.partitioned ? doc.id : null;
         doc.data = doc.data || {};
-        this.client.upsertDocument(`dbs/${this.databaseName}/colls/${this.collectionName}`, doc, { partitionKey:partitionKey, disableAutomaticIdGeneration:true }, next);
+        this.client.upsertDocument(`dbs/${this.databaseName}/colls/${this.collectionName}`, doc, { partitionKey, disableAutomaticIdGeneration: true }, next);
       }, callback);
     }
 
@@ -111,13 +111,13 @@ export class DocumentDbBotStorage implements IBotStorage {
       } else {
         this.initialization = false;
         async.series([
-          (next:DocumentDbCallback) => {
-            this.createDatabaseIfNotExists(next)
+          (next: DocumentDbCallback) => {
+            this.createDatabaseIfNotExists(next);
           },
           (next: (err: QueryError) => void) => this.createCollectionIfNotExists(next),
-        ], (err:any) => {
+        ], (err: any) => {
           callback(err);
-          this.pendingInit.forEach(x => x(err));
+          this.pendingInit.forEach((x) => x(err));
           this.pendingInit.length = 0;
         });
       }
@@ -125,7 +125,7 @@ export class DocumentDbBotStorage implements IBotStorage {
 
     private createDatabaseIfNotExists(callback: DocumentDbCallback): void {
       this.client.createDatabase({id: this.databaseName}, (err) => {
-        callback(err && err.code != HTTP_CONFLICT ? err : null);
+        callback(err && err.code !== HTTP_CONFLICT ? err : null);
       });
     }
 
@@ -133,7 +133,7 @@ export class DocumentDbBotStorage implements IBotStorage {
       const collection = { id: this.databaseName, defaultTtl: this.defaultTtl };
       const collectionOpts = { offerThroughput: this.collectionThroughput };
       this.client.createCollection(`dbs/${this.databaseName}`, collection, collectionOpts, (err) => {
-        callback(err && err.code != HTTP_CONFLICT ? err : null);
+        callback(err && err.code !== HTTP_CONFLICT ? err : null);
       });
     }
 
@@ -149,14 +149,14 @@ export class DocumentDbBotStorage implements IBotStorage {
       return `${this.conversationId(context)};${this.userId(context)}`;
     }
 
-    private mapDocumentDbError<T>(callback: (err: Error, resource:any) => void): (err: QueryError, resource?:T, headers?:any) => void {
-      return (err: QueryError|Error|{message:string}, resource?:T, headers?:any) => {
+    private mapDocumentDbError<T>(callback: (err: Error, resource: any) => void): (err: QueryError, resource?: T, headers?: any) => void {
+      return (err: QueryError|Error|{message: string}, resource?: T, headers?: any) => {
         if (err instanceof Error) {
           callback(err, null);
         } else if (err && err.hasOwnProperty('message')) {
-          callback(new Error((<{message:string}>err).message), null);
+          callback(new Error((err as {message: string}).message), null);
         } else if (err && err.hasOwnProperty('code')) {
-          callback(new Error(`${(<QueryError>err).code}: ${(<QueryError>err).body}`), null)
+          callback(new Error(`${(err as QueryError).code}: ${(err as QueryError).body}`), null);
         } else if (err) {
           callback(new Error('Unknown error'), null);
         } else {
--- src/mock-document-client.ts
+++ src/mock-document-client.ts
@@ -1,65 +1,64 @@
-import { 
-  DocumentClient, DocumentOptions,
-  RequestOptions, RequestCallback,
-  NewDocument, RetrievedDocument,
-  QueryError, UniqueId,
-  DatabaseMeta, Collection,
-  CollectionMeta } from 'documentdb';
-
+import {
+  Collection, CollectionMeta,
+  DatabaseMeta, DocumentClient,
+  DocumentOptions, NewDocument,
+  QueryError, RequestCallback,
+  RequestOptions, RetrievedDocument,
+  UniqueId } from 'documentdb';
 
 export class MockDocumentClient extends DocumentClient {
-  store: {[id:string]:RetrievedDocument<any>} = {};
+  store: {[id: string]: RetrievedDocument<any>} = {};
   args: RequestOptions[] = [];
   database: {body: UniqueId, options?: RequestOptions};
   collection: {body: Collection, options?: RequestOptions};
 
   constructor(private databaseExists: boolean, private collectionExists: boolean) {
-    super('https://mock:443', {masterKey:'123'})
+    super('https://mock:443', {masterKey: '123'});
   }
-  
-  readDocument<T>(documentLink:string, ...args: any[]): void {
+
+  readDocument<T>(documentLink: string, ...args: any[]): void {
     const [options, callback] = this.optionsOrCallback<RequestOptions, RequestCallback<RetrievedDocument<T>>>(args[0], args[1]);
     this.args.push(options);
     if (!this.store.hasOwnProperty(documentLink)) {
-      callback({code:404, body:'not found'}, null, null);
+      callback({code: 404, body: 'not found'}, null, null);
     } else {
       callback(null, this.store[documentLink], {});
     }
   }
 
-  upsertDocument<T>(documentsFeedOrDatabaseLink:string, body:NewDocument<T>, ...args:any[]): void {
+  upsertDocument<T>(documentsFeedOrDatabaseLink: string, body: NewDocument<T>, ...args: any[]): void {
     const [options, callback] = this.optionsOrCallback<DocumentOptions, RequestCallback<RetrievedDocument<T>>>(args[0], args[1]);
-    const doc = <RetrievedDocument<T>>body;
+    const doc = body as RetrievedDocument<T>;
     const docId = `${documentsFeedOrDatabaseLink}/docs/${body.id}`;
     this.args.push(options);
     this.store[docId] = doc;
     callback(null, doc, {});
   }
 
-  createDatabase(body: UniqueId, ...args:any[]): void {
+  createDatabase(body: UniqueId, ...args: any[]): void {
     const [options, callback] = this.optionsOrCallback<RequestOptions, RequestCallback<DatabaseMeta>>(args[0], args[1]);
-    const db = <DatabaseMeta>body;
+    const db = body as DatabaseMeta;
     if (this.databaseExists) {
-      callback({code:409, body:'Exists'}, null, null);
+      callback({code: 409, body: 'Exists'}, null, null);
     } else {
-      this.database = {body: db, options: options}
+      this.database = {body: db, options};
       callback(null, db, {});
     }
   }
 
-  createCollection(databaseLink: string, body: Collection, ...args:any[]): void {
+  createCollection(databaseLink: string, body: Collection, ...args: any[]): void {
     const [options, callback] = this.optionsOrCallback<RequestOptions, RequestCallback<CollectionMeta>>(args[0], args[1]);
-    const coll = <CollectionMeta>body;
+    const coll = body as CollectionMeta;
     if (this.collectionExists) {
-      callback({code:409, body:'Exists'}, null, null);
+      callback({code: 409, body: 'Exists'}, null, null);
     } else {
-      this.collection = {body: coll, options: options}
+      this.collection = {body: coll, options};
       callback(null, coll, {});
     }
   }
 
-  private optionsOrCallback<TOptions, TCallback>(options:any, callback:any): [TOptions, TCallback] {
-    if (typeof(options) == 'function') {
+  private optionsOrCallback<TOptions, TCallback>(options: any, callback: any): [TOptions, TCallback] {
+    if (typeof(options) === 'function') {
       return [null, options];
     } else {
       return [options, callback];
