567c1ee5eb05c886b24f9727324a8500b3e7791b
Chris Stone <chstone@microsoft.com>
2017-04-05 16:56:10 -0400

    nunit results path

--- spec/helpers/reporters.js
+++ spec/helpers/reporters.js
@@ -2,5 +2,5 @@ const path = require('path');
 const jasmineReporters = require('jasmine-reporters');
 
 jasmine.getEnv().addReporter(new jasmineReporters.NUnitXmlReporter({
-  savePath: process.env.COMMON_TESTRESULTSDIRECTORY || path.resolve(__dirname, '..', '..'),
+  savePath: process.env.COMMON_TESTRESULTSDIRECTORY ? path.resolve(process.env.COMMON_TESTRESULTSDIRECTORY, 'nunit') : path.resolve(__dirname, '..', '..'),
 }));
